ace4d909d0dead8e5483206d027741dc83556d07
AdamZsofi <adamzsofi.mail@gmail.com>
2024-10-20 14:31:54 +0200

    fix formatting

--- witness2ast.py
+++ witness2ast.py
@@ -13,6 +13,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 """
+
 import networkx as nx
 from pycparser.c_ast import (
     Compound,
