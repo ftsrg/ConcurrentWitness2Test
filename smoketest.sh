2617541f0f2e100e55768dcf4ea726b7d4ce5f94
Levente Bajczi <bajczilevi@gmail.com>
2025-10-21 16:46:34 +0200

    Added smoketest

--- smoketest.sh
+++ smoketest.sh
@@ -1,1 +1,2 @@
-./start.sh example/mix000.opt.i --witness example/mix000.opt.i.graphml --mode normal<witnessfile>
+#!/bin/bash
+./start.sh example/mix000.opt.i --witness example/mix000.opt.i.graphml --mode permissive
